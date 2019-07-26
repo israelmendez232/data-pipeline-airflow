fefda6c6a6f75ba968b63b5e5e7fc7ec5f4c2268
Israel Mendes <israel.mendez232@gmail.com>
2019-07-26 19:39:16 -0300

    Load_Dimension modifications.

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -3,20 +3,33 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class LoadDimensionOperator(BaseOperator):
+    """
+    
+    
+    
+    """
 
     ui_color = '#80BD9E'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id='',
+                 table='',
+                 sql_query='',
+                 truncate='',
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.sql_query = sql_query
+        self.truncate = truncate
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        self.log.info(' =====> Main configurations in LoadDimensionOperator.')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        if self.truncate:
+            redshift.run(f'TRUNCATE TABLE {self.table}')
+        redshift.run(f'INSERT INTO {self.table} ' + str(self.sql_query))
+        
+        self.log.info(' =====> Done with LoadDimensionOperator!')
